0c42acb2391d2dc65a891334ab97b49dd76766bf
bartick <bartickmaiti1998@gmail.com>
2022-04-07 20:11:40 +0530

    express-session to cookie-session

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const session = require('express-session');
+const session = require('cookie-session');
 
 require('./logic/database').connect();
 require('dotenv').config();
